96d526ec25c8dca28363bd6070a3e90642a77f25
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2026-06-12 13:42:25 -0300

    acceleration and deceleration

--- abyssal-cleaner-3d/character_body_3d.gd
+++ abyssal-cleaner-3d/character_body_3d.gd
@@ -15,13 +15,13 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, SPEED * direction.x, 0.05)
+		velocity.z = move_toward(velocity.z, SPEED * direction.z, 0.05)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 0.05)
 		velocity.z = move_toward(velocity.z, 0, 0.05)
-
+		
 	move_and_slide()
