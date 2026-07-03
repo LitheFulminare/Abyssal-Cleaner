f6245dcfca93a61288c353a3b689291a298f1b44
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2026-07-03 14:48:27 -0300

    organization
    shoot method is on Weapon base class, the implementations decide with version of shoot should be called depending on firing_type

--- abyssal-cleaner-3d/Submarine Parts/Weapons/Cannon/cannon.gd
+++ abyssal-cleaner-3d/Submarine Parts/Weapons/Cannon/cannon.gd
@@ -5,14 +5,21 @@ const CANNON_PROJECTILE = preload("uid://0xuu7gjli1to")
 @export var weapon_stats: WeaponStats
 
 func initialize() -> void:
+	stats = weapon_stats
+	
+	set_firing_mode()
+	
 	spawn_point = %ProjectileSpawnPoint
 	projectile_pool = %ObjectPool
 	projectile_pool.create_pool(CANNON_PROJECTILE, 20)
-	stats = weapon_stats
 	projectile_scene = CANNON_PROJECTILE
+	shoot_delay = 1 / stats.fire_rate
 
-func shoot() -> void:
-	var projectile: Projectile = projectile_pool.acquire()
-	projectile.global_position = spawn_point.global_position
-	projectile.direction = -spawn_point.global_transform.basis.z.normalized()
-	projectile.global_transform.basis = spawn_point.global_transform.basis
+func set_firing_mode() -> void:
+	match stats.firing_type:
+		stats.FIRING_TYPE.AUTOMATIC:
+			current_weapon_firing_mode = shoot_automatic
+		stats.FIRING_TYPE.BURST:
+			current_weapon_firing_mode = shoot_burst
+		stats.FIRING_TYPE.LASER:
+			current_weapon_firing_mode = shoot_laser
--- abyssal-cleaner-3d/Submarine Parts/Weapons/base_weapon.gd
+++ abyssal-cleaner-3d/Submarine Parts/Weapons/base_weapon.gd
@@ -2,25 +2,46 @@
 @abstract class_name Weapon extends Node
 
 var stats: WeaponStats
-
 var projectile_pool: ObjectPool
-
 var spawn_point: Marker3D
-
 var projectile_scene: PackedScene
 
-## Whether the weapons has overheat or ammo mechanic.
-var can_overheat: bool = true
+var cooldown: float
+var can_fire: bool = true
+var shoot_delay: float
+
+var current_weapon_firing_mode: Callable
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if can_fire == false:
+		cooldown += delta
+		if cooldown > shoot_delay:
+			can_fire = true
+			cooldown = 0
 
 @abstract
-## Override this funcion instead of ready()
+## Set stats, projectile_pool, spawn_point and projectile_scene.
 func initialize() -> void
 
-@abstract
-func shoot() -> void
+func shoot() -> void:
+	current_weapon_firing_mode.call()
+
+func shoot_automatic() -> void:
+	if !can_fire:
+		return
+	
+	var projectile: Projectile = projectile_pool.acquire()
+	projectile.global_position = spawn_point.global_position
+	projectile.direction = -spawn_point.global_transform.basis.z.normalized()
+	projectile.global_transform.basis = spawn_point.global_transform.basis
+	
+	can_fire = false
+
+func shoot_burst() -> void:
+	print("Burst shot (method not implemented)")
+	
+func shoot_laser() -> void:
+	print("Laser shot (method not implemented)")
